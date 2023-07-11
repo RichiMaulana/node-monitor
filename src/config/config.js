8108b6a6970154ee13de605c05079db74cc2c222
RichiMaulana <richimaulana30@gmail.com>
2023-07-11 10:09:35 +0700

    adjust: db config default value

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: env.DB_NAME,
     username: env.DB_USER,
     password: env.DB_PASSWORD,
-    dialect: env.DB_TYPE,
+    dialect: env.DB_TYPE || "mysql",
     host: env.DB_HOST,
     port: env.DB_PORT,
     define: {
@@ -18,12 +18,12 @@ module.exports = {
     database: env.DB_NAME_PROD,
     username: env.DB_USER_PROD,
     password: env.DB_PASSWORD_PROD,
-    dialect: env.DB_TYPE_PROD,
+    dialect: env.DB_TYPE_PROD || "mysql",
     host: env.DB_HOST,
     port: env.DB_PORT,
     define: {
       timestamps: false,
-      logging: false
+      logging: false,
     },
   },
 };
