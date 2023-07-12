2b489d6911ee1bed6cb888c8f6dcec7ad9933f2e
RichiMaulana <richimaulana30@gmail.com>
2023-07-12 10:46:37 +0700

    add service file and adjust db logging

--- src/config/config.js
+++ src/config/config.js
@@ -10,20 +10,21 @@ module.exports = {
     dialect: env.DB_TYPE || "mysql",
     host: env.DB_HOST,
     port: env.DB_PORT,
+    logging: env.DB_LOGGING === 'true' ? true : false,
     define: {
       timestamps: false,
     },
   },
   production: {
-    database: env.DB_NAME_PROD,
-    username: env.DB_USER_PROD,
-    password: env.DB_PASSWORD_PROD,
-    dialect: env.DB_TYPE_PROD || "mysql",
+    database: env.DB_NAME,
+    username: env.DB_USER,
+    password: env.DB_PASSWORD,
+    dialect: env.DB_TYPE || "mysql",
     host: env.DB_HOST,
     port: env.DB_PORT,
+    logging: false,
     define: {
       timestamps: false,
-      logging: false,
     },
   },
 };
